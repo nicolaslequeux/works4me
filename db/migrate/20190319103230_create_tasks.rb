fbba6da9e663469972e98c4d09efaf07be211f70
nicolas lequeux <contact@nicolaslequeux.com>
2019-03-19 11:34:25 +0100

    migrate models

---
+++ db/migrate/20190319103230_create_tasks.rb
@@ -0,0 +1,19 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :category
+      t.text :description
+      t.integer :price
+      t.string :address
+      t.string :status
+      t.string :picture
+      t.integer :owner_user_id
+      t.integer :worker_user_id
+
+      t.timestamps
+    end
+    add_index :tasks, :owner_user_id
+    add_index :tasks, :worker_user_id
+  end
+end
