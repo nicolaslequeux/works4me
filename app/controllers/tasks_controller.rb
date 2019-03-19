c9d22a276eb8a8b90db6318fcb05ffc78f73e862
giacomo montemurri <gia94.montemurri@yahoo.it>
2019-03-19 14:51:54 +0100

    tasks controller ongoing

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.owner_user_id = current_user
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :category, :description, :price, :address, :status, :picture)
+  end
+end
