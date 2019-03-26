c4067fc94acf194c9832ad98bc2dda9d892f82aa
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-03-26 18:13:44 +0100

    disabling mails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  after_create :send_welcome_email
+  # MAILER !!! UNCOMMENT BEFORE PRODUCTION
+  # after_create :send_welcome_email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :omniauthable, omniauth_providers: [:facebook]
